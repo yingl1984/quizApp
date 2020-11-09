106a614ef2eb1882dfea6d0f75f466951459d842
Ying Liu <yingl1984@gmail.com>
2020-11-09 08:55:15 -0800

    questionrender function, start listerner

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
 /**
  * Example store structure
  */
-const store = {
+const STORE = {
   // 5 or more questions are required
-  questions: [
-    {
-      question: 'What color is broccoli?',
-      answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
-      ],
-      correctAnswer: 'green'
+  questions: 
+  [
+    { title: "Which of these games came first?" , 
+    choices: ["Super Mario World", "Super Mario Bros", "Super Mario Land", "Mario Bros"],
+    correctIndex: 2,
     },
     {
-      question: 'What is the current year?',
-      answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
-      correctAnswer: '2019'
+      title: "Which of these sentences is true about Mario and Luigi?",
+      choices: ["Mario is taller and Luigi wears red", "Mario is shorter and Luigi wears red", "Mario is taller and Luigi wears green", "Mario is shorter and Luigi wears green"],
+      correctIndex: 4
+    },
+    {
+      title: "Mario went 3D in Super Mario 64, but in what year?",
+      choices: ["1994", "1995", "1996", "1997"],
+      correctIndex: 3
+    },
+    {
+      title: "What is the actual name of the raccoon-y outfit Mario wears?",
+      choices: ["Kabooki suit", "Zanoosi suit", "Tanooki suit", "Adooki suit"],
+      correctIndex: 3
+    },
+    {
+      title: "What is Princess Peach also known as?",
+      choices: ["Princess Toadstool", "Princess Fungus", "Princess Mushroom", "Princess Portobello"],
+      correctIndex: 1
     }
   ],
   quizStarted: false,
-  questionNumber: 0,
-  score: 0
+  currentQuestionNum: 0,
+  score: 0,
+  hasFeedback: false
 };
 
 /**
@@ -50,9 +56,48 @@ const store = {
 // These functions return HTML templates
 
 /********** RENDER FUNCTION(S) **********/
+function render(){
+  $('#start').hide();
+  $('#questions').hide();
 
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
+  if(!STORE.started){
+    $('#start').show();
+  }else if(STORE.currentQuestionNum < STORE.questions.length){
+    renderQuestion();
+  }else{
+    renderSummary();
+  }
+}
 
+function renderQuestion(){
+  $('#questions').show();
+  const curQue = STORE.questions[STORE.currentQuestionNum];
+  //Fill the question into the <h1>
+  $('#questions h2').text(curQue.title);
+  for(let i = 0; i < curQue.choices.length; i++){
+    $('#choices').append(`
+      <input type="radio", name="choice", value="${i}", id="${i}">
+      <label for="${i}">${curQue.choices[i]}</label>
+    `
+    );
+  }
+}
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+function startQuiz(){
+  $('#start-quiz').click(even => {
+    console.log("start button works");
+    STORE.quizStarted = true;
+    render();
+  })
+}
+
+
+// logical procedure 
+function quiz(){
+  startQuiz();
+  render();
+}
+
+$(quiz);
